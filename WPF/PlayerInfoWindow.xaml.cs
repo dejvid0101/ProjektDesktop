259af0f3a2e20a536edd6192d9b937f49927e620
David Koren <dejvid0101@gmail.com>
2022-08-14 14:13:08 +0200

    backup commit 1.7

--- WPF/PlayerInfoWindow.xaml.cs
+++ WPF/PlayerInfoWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,6 +30,7 @@ namespace WPF
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+
             string z = DAL1.TextAccess.readFile(@"..\..\..\DAL1\Files\PlayerInfoHelper.txt");
             lblName.Content = z;
             IList<DAL1.QuickType.Tekma> list = null;
@@ -39,14 +41,82 @@ namespace WPF
             {
 
                 list = DAL1.APIAccessTeams.GetData2(api);
-            } else
+            }
+            else
             {
                 list = DAL1.APIAccessTeams.GetData2(api2);
             }
 
+            int goalscntr = 0;
+            int yellowcntr = 0;
+
             string r = DAL1.TextAccess.readFile(@"..\..\..\DAL1\Files\PlayerInfoHelper.txt");
             foreach (var item in list)
             {
+               
+                            foreach (var l in item.AwayTeamEvents)
+                            {
+                                if (l.TypeOfEvent == DAL1.QuickType.TypeOfEvent.Goal)
+                                {
+                                    if (lblName.Content.ToString().Trim() == l.Player)
+                                    {
+                                        goalscntr++;
+                                    }
+
+                                }
+                            }
+                            foreach (var l in item.HomeTeamEvents)
+                            {
+                                if (l.TypeOfEvent == DAL1.QuickType.TypeOfEvent.Goal)
+                                {
+                                    if (lblName.Content.ToString().Trim() == l.Player)
+                                    {
+                                        goalscntr++;
+                                    }
+
+                                }
+                            }
+
+                        }
+                    
+                    
+                        foreach (var item in list)
+                        {
+                            foreach (var l in item.AwayTeamEvents)
+                            {
+                                if (l.TypeOfEvent == DAL1.QuickType.TypeOfEvent.YellowCard)
+                                {
+                                    if (lblName.Content.ToString().Trim() == l.Player)
+                                    {
+                                        yellowcntr++;
+                                    }
+
+                                }
+                            }
+                            foreach (var l in item.HomeTeamEvents)
+                            {
+                                if (l.TypeOfEvent == DAL1.QuickType.TypeOfEvent.YellowCard)
+                                {
+                                    if (lblName.Content.ToString().Trim() == l.Player)
+                                    {
+                                        yellowcntr++;
+                                    }
+
+                                }
+                            }
+
+                        }
+            lblGoals.Content = "Ukupno golova: " + goalscntr;
+            lblYellow.Content = "Ukupno žutih kartona: " + yellowcntr;
+
+
+
+
+            foreach (var item in list)
+            {
+
+
+
                 for (int i = 0; i < item.AwayTeamStatistics.StartingEleven.Length; i++)
                 {
                     if (item.AwayTeamStatistics.StartingEleven[i].Name == r)
@@ -57,7 +127,7 @@ namespace WPF
 
                         if (!item.AwayTeamStatistics.StartingEleven[i].Captain)
                         {
-                            lblCaptain.Content = "";
+                            lblCaptain.Content = "Team member";
 
                         }
                         else if (item.AwayTeamStatistics.StartingEleven[i].Captain)
@@ -72,12 +142,45 @@ namespace WPF
 
                 }
 
-               
+
 
 
             }
 
-            
+            string[] files = Directory.GetFiles(@"..\..\..\DAL1\Images\");
+
+            foreach (string file in files)
+            {
+
+                if (lblName.Content.ToString().Trim() == file.GetUntilOrEmpty().Trim())
+                {
+                    imgBox.Source = new BitmapImage(new Uri(@"C:\Users\David\OneDrive - Visoko uciliste Algebra\Desktop\ProjektDesktop\DAL1\Images\"+ lblName.Content.ToString().Trim() +".png"));
+                }
+            }
+
+
+
         }
+        
+    }
+    static class Helper
+    {
+        public static string GetUntilOrEmpty(this string text, string stopAt = ".")
+        {
+            if (!String.IsNullOrWhiteSpace(text))
+            {
+                int charLocation = text.LastIndexOf(stopAt, StringComparison.Ordinal) - 21;
+
+
+                if (charLocation > 0)
+                {
+                    return text.Substring(21, charLocation);
+                }
+            }
+
+            return String.Empty;
+        }
+
+
     }
 }
